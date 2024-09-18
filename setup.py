09dd847ee2aba45941a4a0d3fc2773b247ccec62
Henri Drake <henrifdrake@gmail.com>
2024-09-18 15:13:51 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='gfdl_utils',
     url='https://github.com/hdrake/gfdl_utils.git',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     license='MIT',
 )
